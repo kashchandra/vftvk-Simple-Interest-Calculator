af4f867445994d30f3a85cea5899fc8f95fe9bc8
kashchandra <83214742+kashchandra@users.noreply.github.com>
2021-06-19 19:34:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+  
+function check() {
+  var prin= document.myform.principal.value;
+  if(prin == 0 || prin < 0) {
+    alert("enter a positive number");
+    setTimeout((function(){document.myform.principal.value="";document.myform.principal.focus();}),2 );
+    return false;
+  }
+  return true;
+};
+function compute() {
+  var  p = document.getElementById("principal").value;
+  var  r= document.getElementById("rate").value;
+  var  t= document.getElementById("years").value;
+  var interest = (p*r*t)/100;
+  var year= new Date().getFullYear() + parseInt(t);
+  document.getElementById("result").innerHTML= "If you deposit <span class=highlight>"+p+"</span>,<br>at an interest rate of <span class=highlight> "+r+"%</span>.<br>You will receive an amount of <span class=highlight>"+interest+"</span>,<br>in the year <span class=highlight>"+year+"</span>.<br><br>"; 
 }
+function updaterate() {
+    var rateval= document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+
         
